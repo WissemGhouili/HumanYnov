3c8fa948a6a9461a06b66ff4818b83b172da5c7b
Wissem GHOUILI <72556219+WissemGhouili@users.noreply.github.com>
2023-11-29 11:50:08 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Human Ynov</title>
+  </head>
+  <body>
+    <h1> Human Ynov, la solidarité au coeur de votre campus </h1>
+  </body>
+</html>
